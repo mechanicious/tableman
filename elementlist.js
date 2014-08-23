948410e02cfb2a8893cb6f84f08eb9608132bb9d
Mechanic <zuires@gmail.com>
2014-08-23 19:15:35 +0200

    docs

--- elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","mechanicious\\Columnizer\\Column"],["c","mechanicious\\Columnizer\\ColumnBag"],["c","mechanicious\\Columnizer\\Columnizer"],["c","mechanicious\\Extensions\\Bs3Table\\Bs3Table"],["c","mechanicious\\Tableman\\Tableman"],["c","mechanicious\\TablemanExtension\\Config"],["c","mechanicious\\TablemanExtension\\TablemanExtension"],["c","mechanicious\\TablemanExtension\\TablemanExtensionMakeInterface"],["c","mechanicious\\Test\\Other"],["c","mechanicious\\Test\\Tests"]];
